Fixed formula error in theoretical derivative of partial(loss)/partial(U)

nn-using-only-numpy/computational_utils.py
@@ -151,7 +151,7 @@ class Utilities:
 
         if print_debug:
             logger.debug('u_times_x_one_hot=\n%s\n' % u_times_x_one_hot)
-            logger.debug('new_state_vector=\n%s\n' % new_state_vector)
+            logger.debug('loss_from_matrix_u() new_state_vector=\n%s\n' % new_state_vector)
             logger.debug('logits_vector=\n%s\n' % logits_vector)
             logger.debug('softmax_vector=\n%s\n' % Utilities.softmax(logits_vector))
 
@@ -175,7 +175,7 @@ class Utilities:
         assert input_x_integer >= 0 and input_x_integer < vocab_dim
 
         u_times_x_one_hot = matrix_u_0[:, input_x_integer]
-        before_tanh_vector = prev_s_times_w_result_vector + prev_s_times_w_result_vector
+        before_tanh_vector = prev_s_times_w_result_vector + u_times_x_one_hot
 
         new_state_vector = np.tanh(before_tanh_vector)
         logits_vector = np.matmul(matrix_v, new_state_vector)
@@ -202,7 +202,7 @@ class Utilities:
         if print_debug:
             logger.debug('u_times_x_one_hot=\n%s\n' % u_times_x_one_hot)
             logger.debug('before_tanh_vector=\n%s\n' % before_tanh_vector)
-            logger.debug('new_state_vector=\n%s\n' % new_state_vector)
+            logger.debug('loss_from_matrix_u_derivative_wrt_u() new_state_vector=\n%s\n' % new_state_vector)
             logger.debug('logits_vector=\n%s\n' % logits_vector)
             logger.debug('probs_minus_y_one_shot=\n%s\n' % probs_minus_y_one_shot)
             logger.debug('partial_loss_partial_new_state=\n%s\n' % partial_loss_partial_new_state)

nn-using-only-numpy/derivative_verifier.py
@@ -596,20 +596,20 @@ class TestRNNDerivatives(unittest.TestCase):
         # vocab dim = 4
 
         matrix_u = np.array([
-            [.1, .2, .3, .4],
-            [.11, .8, .33, .44],
-            [.111, 10.222, .333, .444],
+            [0, .2, .3, .4],
+            [0, .8, .33, .44],
+            [1.0, 10.222, .333, .444],
         ])
         matrix_v = np.array([
-            [9, 1, 2],
-            [1, 8, 3],
-            [0., 1.5, 7],
-            [4, 5, 1],
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 20],
         ])
-        prev_s_times_w_result_vector = np.array([.8, .2, .1])
+        prev_s_times_w_result_vector = np.array([0, 0, .1])
 
-        input_x_integer = 1
-        label_y_integer = 0
+        input_x_integer = 0
+        label_y_integer = 3
         test = DerivativeVerifier.check_theoretical_derivative_equals_numerical_diff(
             lambda u: Utilities.loss_from_matrix_u(input_x_integer, u, prev_s_times_w_result_vector, matrix_v, label_y_integer, print_debug=True),
             lambda u: Utilities.loss_from_matrix_u_derivative_wrt_u(input_x_integer, u, prev_s_times_w_result_vector, matrix_v,
@@ -636,10 +636,20 @@ class TestRNNDerivatives(unittest.TestCase):
             label_y_integer = np.random.randint(4)
 
             test = DerivativeVerifier.check_theoretical_derivative_equals_numerical_diff(
-                lambda u: Utilities.loss_from_matrix_u(input_x_integer, u, prev_s_times_w_result_vector, matrix_v, label_y_integer, print_debug=True),
+                lambda u: Utilities.loss_from_matrix_u(input_x_integer, u, prev_s_times_w_result_vector, matrix_v, label_y_integer, print_debug=False),
                 lambda u: Utilities.loss_from_matrix_u_derivative_wrt_u(input_x_integer, u, prev_s_times_w_result_vector, matrix_v,
-                    label_y_integer, print_debug=True),
-                matrix_u, error_relative_to_delta_x=1, print_diff=True)
+                    label_y_integer, print_debug=False),
+                matrix_u, error_relative_to_delta_x=1, print_diff=False)
+
+            partial_loss_partial_u = Utilities.loss_from_matrix_u_derivative_wrt_u(input_x_integer, matrix_u, prev_s_times_w_result_vector, matrix_v,
+                    label_y_integer, print_debug=False),
+            numerical_jacobian_diff_matrix = DerivativeVerifier.numerical_jacobian_diff_matrix(
+                lambda u: Utilities.loss_from_matrix_u(input_x_integer, u, prev_s_times_w_result_vector, matrix_v, label_y_integer, print_debug=False),
+                matrix_x_0=matrix_u,delta_x_scalar=1e-7)
+
+            logger.debug('theoretical partial(loss)/partial(U)=\n%s\n' % partial_loss_partial_u)
+            logger.debug('numerical diff jacobian/delta=\n%s\n' % (numerical_jacobian_diff_matrix / 1e-7))
+
             self.assertTrue(test)
 
 