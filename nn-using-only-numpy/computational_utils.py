Fixed derivative of tanh. It should be 1/cosh^2, I put it as 1/cosh

nn-using-only-numpy/computational_utils.py
@@ -187,8 +187,8 @@ class Utilities:
         # this equals (probs_vector - y_one_hot_vector) times matrix_v. See my notes for more detailed calculations.
         partial_loss_partial_new_state = np.matmul(probs_minus_y_one_shot, matrix_v)
 
-        # derivative of tanh is sech
-        sech_before_tanh_vector = 1 / np.cosh(before_tanh_vector)
+        # derivative of tanh is sech ^ 2
+        sech_before_tanh_vector = 1 / (np.cosh(before_tanh_vector) ** 2)
 
         # partial(loss) / partial(U(j, k)) = 0 when k != x. This is non-zero only for U(j, input_x_integer), a non-zero column.
         # use np.multiply here for a member-wise multiplication.

nn-using-only-numpy/rnn_using_only_numpy.py
@@ -146,7 +146,7 @@ class RnnWithNumpy:
         # Computing the Jacobian matrix of: partial(loss)/partial(matrix_u)
         # See comments within computational_utils.py, loss_from_matrix_u_derivative_wrt_u()
         partial_loss_partial_new_state = np.matmul(probs_minus_y_one_shot, matrix_v)
-        sech_before_activation_state_vector = 1 / np.cosh(current_state_before_activation)
+        sech_before_activation_state_vector = 1 / (np.cosh(current_state_before_activation) ** 2)
         partial_loss_partial_before_activation_vector = np.multiply(partial_loss_partial_new_state, sech_before_tanh_vector)
 
         # According to my notes, partial(loss)/partial(u_at_column_x) equals to partial_loss_partial_before_activation_vector times 1