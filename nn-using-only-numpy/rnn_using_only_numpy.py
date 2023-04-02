7297b3158f67818c615221d4b420ab21ecb08e71
Shuishi Yang <shuishiyang@Shuishis-MacBook-Pro.local>
2023-04-02 22:07:20 +0800

    Add untested partial_state_time_t_partial_matrix_w

--- nn-using-only-numpy/rnn_using_only_numpy.py
+++ nn-using-only-numpy/rnn_using_only_numpy.py
@@ -179,6 +179,56 @@ class RnnWithNumpy:
             # but for forward_computation_intermediates_array[t].current_state, it's a function of forward_computation_intermediates_array[t-1].current_state and W
 
 
+    # Returns a Jacobian matrix of partial(state_vector@time_t) / partial(matrix_w)
+    # See my notes, due to state_vector@time_t is a function of F(W, state_vector@time_t_minus_1), this will be calculated recursively.
+    @staticmethod
+    def partial_state_time_t_partial_matrix_w(states_array_indexed_by_time, current_time, dim_hidden, matrix_w, truncation_len=10, check_shapes=True):
+        if truncation_len <= 0 or current_time <= 0
+            # TODO: check if just a simple zero works, or should I need a size-aligned three dimensional zero matrix.
+            return 0
+
+        if check_shapes:
+            assert matrix_w.shape = (dim_hidden, dim_hidden)
+
+        current_state = states_array_indexed_by_time[current_time]
+
+        if check_shapes:
+            assert current_state.ndim == 1 and current_state.size == dim_hidden
+
+        # state = tanh(state_raw)
+        # partial(state)/partial(state_raw) = d(tanh)/d(x) = 1 - tanh(x)^2 = 1 - state^2
+        diag_matrix_partial_state_partial_state_raw = np.diag(1 - current_state ** 2)
+
+        # in the following code, f_t means a function that takes W, state_vector_t_minus_1 as a parameter, and outputs multiply(W, state_vector_t_minus_1)
+        # partial_f_partial_param_1 is the partial derivative wrt to the first parameter (W), and partial_f_partial_2 is the partial derivative wrt to the second parammeter
+        # Note that state_vector_t_minus_1 is also a function of (W, state_vector_t_minus_2)
+        # So partial(F)/partial(W) = partial(F)/parital(F parameter 1) + partial(F)/partial(F parameter 2) * partial(state_vector_t_minus_1) / partial(W)
+
+        def _create_partial_f_partial_param_1(prev_state_vector):
+            if check_shapes:
+                assert prev_state_vector.ndim == 1 and prev_state_vector.size == dim_hidden
+            partial_f_partial_param_1 = np.full([dim_hidden, dim_hidden], None, dtype=object)
+            for i in range(hidden_dim):
+                for j in range(hidden_dim):
+                    partial_f_partial_param_1[i][j] = np.zeros().T
+                    partial_f_partial_param_1[i][j][i] = prev_state_vector[j]
+
+            return partial_f_partial_param_1
+
+        # current_time == 0 already checked.
+        partial_f_partial_param_1 = _create_partial_f_partial_param_1(states_array_indexed_by_time[current_time - 1])
+
+        # See my notes.
+        partial_f_partial_param_2 = matrix_w
+
+        partial_prev_state_partial_matrix_w = partial_state_time_t_partial_matrix_w(
+            states_array_indexed_by_time, current_time - 1, dim_hidden, matrix_w, truncation_len - 1, check_shapes)
+
+        partial_state_raw_partial_w = partial_f_partial_param_1 + np.matmul(partial_f_partial_param_2, partial_prev_state_partial_matrix_w)
+        partial_state_partial_w = np.matmul(diag_matrix_partial_state_partial_state_raw, partial_state_raw_partial_w)
+        return partial_state_partial_w
+
+
     def step_parameters(self, loss_gradient_u_v_w, step_size, check_shapes=True):
         (partial_loss_partial_u, partial_loss_partial_v, partial_loss_partial_w) = loss_gradient_u_v_w
         if check_shapes:
