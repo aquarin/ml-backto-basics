dbedc438795154e8c12283dd8c4a03a51728df1d
Shuishi Yang <shuishiyang@Shuishis-MacBook-Pro.local>
2023-04-04 00:37:26 +0800

    Save progresses

--- nn-using-only-numpy/rnn_using_only_np_test.py
+++ nn-using-only-numpy/rnn_using_only_np_test.py
@@ -201,13 +201,6 @@ class RnnUsingOnlyNumpyTest(unittest.TestCase):
             return state_time_series[-1]
 
 
-        numerical_jacobian_diff = DerivativeVerifier.numerical_jacobian_diff_matrix(_forward_prop_wrapper, matrix_w, delta_x_scalar)
-        logger.debug('numerical_jacobian_diff=\n%s\n' % numerical_jacobian_diff)
-        numerical_jacobian_derivative = numerical_jacobian_diff / delta_x_scalar
-        logger.debug('numerical_jacobian_derivative=\n%s\n' % numerical_jacobian_derivative)
-        logger.debug('numerical_jacobian_derivative shape=%s' % str(numerical_jacobian_derivative.shape))
-        logger.debug('numerical_jacobian_derivative[0][0] shape=%s' % str(numerical_jacobian_derivative[0][0].shape))
-
         debug_output_dict = {}
         state_time_series = self.forward_with_only_state_vector(
                 dim_hidden, dim_vocab, state_vector_time_negative_1, matrix_w, matrix_u, input_x_integers_by_time)
@@ -216,8 +209,15 @@ class RnnUsingOnlyNumpyTest(unittest.TestCase):
         bptt_partial_state_partial_w_at_time_2 = RnnWithNumpy.bptt_partial_state_time_t_partial_matrix_w(
             state_time_series, current_time=2, dim_hidden=dim_hidden,
             matrix_w = matrix_w, truncation_len=10, check_shapes=True, debug_output_dict=debug_output_dict, print_debug=True)
-        logger.debug('bptt_partial_state_partial_w_at_time_2=\n%s\n' % bptt_partial_state_partial_w_at_time_2)
         logger.debug('debug_output_dict=\n%s\n' % debug_output_dict)
+        logger.debug('bptt_partial_state_partial_w_at_time_2=\n%s\n' % bptt_partial_state_partial_w_at_time_2)
+
+        numerical_jacobian_diff = DerivativeVerifier.numerical_jacobian_diff_matrix(_forward_prop_wrapper, matrix_w, delta_x_scalar)
+        numerical_jacobian_derivative = numerical_jacobian_diff / delta_x_scalar
+        logger.debug('numerical_jacobian_derivative=\n%s\n' % numerical_jacobian_derivative)
+        logger.debug('numerical_jacobian_derivative shape=%s' % str(numerical_jacobian_derivative.shape))
+        logger.debug('numerical_jacobian_derivative[0][0] shape=%s' % str(numerical_jacobian_derivative[0][0].shape))
+
 
         for index, sub_matrix in np.ndenumerate(numerical_jacobian_derivative):
             np.testing.assert_almost_equal(sub_matrix, bptt_partial_state_partial_w_at_time_2[index], 5)
@@ -237,7 +237,7 @@ class RnnUsingOnlyNumpyTest(unittest.TestCase):
             [0, .5, 0, .2],
             [0, 0, .5, .2],
         ])
-        input_x_integers_by_time = [1, 3, 0, 1, 2, 0, 2, 2, 2]
+        input_x_integers_by_time = [1, 3, 0, 1, 2, 0, 2, 2]
         delta_x_scalar = 1e-5
 
         # Only takes a matrix_w parameter so that this one parameter will be diff-ed for numerical derivative.
@@ -264,7 +264,7 @@ class RnnUsingOnlyNumpyTest(unittest.TestCase):
         logger.debug('numerical_jacobian_derivative[0][0] shape=%s' % str(numerical_jacobian_derivative[0][0].shape))
 
         for index, sub_matrix in np.ndenumerate(numerical_jacobian_derivative):
-            np.testing.assert_almost_equal(sub_matrix, bptt_partial_state_partial_w_at_time_2[index], 5)
+            np.testing.assert_almost_equal(sub_matrix, bptt_partial_state_partial_w_at_time_n[index], 5)
 
 
 if __name__ == '__main__':
